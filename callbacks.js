f4b544380ef0642d2023c92c3cda24016b60dd8d
Marco Sotomayor <msotomayor@dc.uba.ar>
2020-01-06 19:16:29 -0300

    Manejo de errores con Callbacks

--- callbacks.js
+++ callbacks.js
@@ -4,21 +4,27 @@ const OPTS = { crossDomain: true}
 
 function getCharacter(id, callback){
     const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $.get(url, OPTS, ({name}) => {
-        console.log(`Hi I'm ${name}`)
+    $
+        .get(url, OPTS, callback)
+        .fail( () => {
+            console.log(`Error. We could get the ${id}th character`)
     })
-
-    if (callback) callback()
 }
 
-getCharacter(1, () => {
-    getCharacter(2, () => {
-        getCharacter(3, () => {
-            getCharacter(4, () => {
-                getCharacter(5, () => {
-                    getCharacter(6, () => {
-                        getCharacter(7, () => {
-                            getCharacter(8)
+getCharacter(1, (person) => {
+    console.log(`Hello I'm ${person.name}`)
+    getCharacter(2, (person) => {
+        console.log(`Hello I'm ${person.name}`)
+        getCharacter(3, (person) => {
+            console.log(`Hello I'm ${person.name}`)
+            getCharacter(4, (person) => {
+                console.log(`Hello I'm ${person.name}`)
+                getCharacter(5, (person) => {
+                    console.log(`Hello I'm ${person.name}`)
+                    getCharacter(6, (person) => {
+                        console.log(`Hello I'm ${person.name}`)
+                        getCharacter(7, (person) => {
+                            console.log(`Hello I'm ${person.name}`)
                         })
                     })
                 })
