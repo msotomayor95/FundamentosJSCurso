a0b5787b9cabd78224699ecdafc9aba9d09e30ef
Marco Sotomayor <msotomayor@dc.uba.ar>
2020-01-06 23:56:02 -0300

    Asyc Await

--- callbacks.js
+++ callbacks.js
@@ -17,13 +17,18 @@ function onError(id) {
     console.log (`Error while getting the character number ${id}`)
 }
 
-var ids = [1, 2, 3, 4, 5, 6, 7]
-var promises = ids.map(id => getCharacter(id))
+async function getCharacters() {
+    var ids = [1, 2, 3, 4, 5, 6, 7]
+    var promises = ids.map(id => getCharacter(id))
+    try{
+        var characters = await Promise.all(promises)
+        console.log(characters)
+    } catch (id){
+        onError(id)
+    }
+}
 
-Promise
-    .all(promises)
-    .then(character => console.log(character))
-    .catch(onError)
+getCharacters();
 
 // getCharacter(1)
 //     .then((character) => {
