633638f2cc387f25a341f19ad7a9ff5bab60cd0b
Marco Sotomayor <msotomayor@dc.uba.ar>
2020-01-06 23:37:11 -0300

    Promesas Encadenadas

--- callbacks.js
+++ callbacks.js
@@ -3,35 +3,62 @@ const PEOPLE_URL = 'people/:id'
 const OPTS = { crossDomain: true}
 
 function getCharacter(id, callback){
-    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
-    $
-        .get(url, OPTS, callback)
-        .fail( () => {
-            console.log(`Error. We could get the ${id}th character`)
+    return new Promise((resolve, reject) => {
+        const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`
+        $
+            .get(url, OPTS, data => {
+                resolve(data)
+            })
+            .fail( () => reject(id))
     })
 }
 
-getCharacter(1, (person) => {
-    console.log(`Hello I'm ${person.name}`)
-    getCharacter(2, (person) => {
-        console.log(`Hello I'm ${person.name}`)
-        getCharacter(3, (person) => {
-            console.log(`Hello I'm ${person.name}`)
-            getCharacter(4, (person) => {
-                console.log(`Hello I'm ${person.name}`)
-                getCharacter(5, (person) => {
-                    console.log(`Hello I'm ${person.name}`)
-                    getCharacter(6, (person) => {
-                        console.log(`Hello I'm ${person.name}`)
-                        getCharacter(7, (person) => {
-                            console.log(`Hello I'm ${person.name}`)
-                        })
-                    })
-                })
-            })
-        })
+function onError(id) {
+    console.log (`Error while getting the character number ${id}`)
+}
+
+getCharacter(1)
+    .then((character) => {
+        console.log(`The character number 1 is ${character.name}`)
+        return getCharacter(2)
     })
-})
+    .then((character) => {
+        console.log(`The character number 2 is ${character.name}`)
+        return getCharacter(3)
+    })
+    .then((character) => {
+        console.log(`The character number 3 is ${character.name}`)
+        return getCharacter(4)
+    })
+    .then((character) => {
+        console.log(`The character number 4 is ${character.name}`)
+        return getCharacter(5)
+    }).then((character) => {
+        console.log(`The character numer 5 is ${character.name}`)
+    })
+    .catch(onError)
+
+// getCharacter(1, (person) => {
+//     console.log(`Hello I'm ${person.name}`)
+//     getCharacter(2, (person) => {
+//         console.log(`Hello I'm ${person.name}`)
+//         getCharacter(3, (person) => {
+//             console.log(`Hello I'm ${person.name}`)
+//             getCharacter(4, (person) => {
+//                 console.log(`Hello I'm ${person.name}`)
+//                 getCharacter(5, (person) => {
+//                     console.log(`Hello I'm ${person.name}`)
+//                     getCharacter(6, (person) => {
+//                         console.log(`Hello I'm ${person.name}`)
+//                         getCharacter(7, (person) => {
+//                             console.log(`Hello I'm ${person.name}`)
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
 
 // getCharacter(2)
 // getCharacter(3)
